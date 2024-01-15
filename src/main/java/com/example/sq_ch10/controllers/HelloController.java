1a980a47d0f6c0417154bfa42e65a8582a22cba5
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-14 20:09:03 -0500

    Update HelloController using the @RestController annotation

--- src/main/java/com/example/sq_ch10/controllers/HelloController.java
+++ src/main/java/com/example/sq_ch10/controllers/HelloController.java
@@ -1,20 +1,17 @@
 package com.example.sq_ch10.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "Hello!";
     }
 
     @GetMapping("/ciao")
-    @ResponseBody
     public String ciao() {
         return "Ciao!";
     }
