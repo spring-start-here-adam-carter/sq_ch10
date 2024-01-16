61261919304dffb538af0406107d833f7c1ac69d
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-15 19:02:37 -0500

    Demonstrate RestControllerAdvice to simplify controller action logic

--- src/main/java/com/example/sq_ch10/controllers/PaymentController.java
+++ src/main/java/com/example/sq_ch10/controllers/PaymentController.java
@@ -19,18 +19,10 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity
-                    .badRequest()
-                    .body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
---
+++ src/main/java/com/example/sq_ch10/controllers/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.example.sq_ch10.controllers.advice;
+
+import com.example.sq_ch10.exceptions.NotEnoughMoneyException;
+import com.example.sq_ch10.model.ErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+}
